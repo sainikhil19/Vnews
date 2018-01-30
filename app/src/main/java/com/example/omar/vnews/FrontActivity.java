5738ed88c349466b7fb31a0692c4a6d2f90e74dd
arrow2851 <omar.h.mohiuddin@gmail.com>
2018-01-30 23:06:21 +0530

    Commented minor issued for working build
    some parts like app:tabIndicatorColor  were not recognized so, for the time being I've commented them as not yet implemented.
    
    Also, i've added some commented code in the FrontActivity that'll most likely be required after an adapter for Tags is implemented

--- app/src/main/java/com/example/omar/vnews/FrontActivity.java
+++ app/src/main/java/com/example/omar/vnews/FrontActivity.java
@@ -1,7 +1,10 @@
 package com.example.omar.vnews;
 
+import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FrontActivity extends AppCompatActivity {
 
@@ -9,5 +12,36 @@ public class FrontActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front);
+        /*
+        uncomment after implementing tagslist adapter
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(DailyActivity.this, EditorActivity.class);
+                             startActivity(intent);
+            }
+        });
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        TagAdapter adapter = new TagAdapter(this, getSupportFragmentManager());
+
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+
+        // Find the tab layout that shows the tabs
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        */
+
+
     }
 }
